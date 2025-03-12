a34729576bb33e298378145d57d3b83de0f956f2
YeongDae <dldudeo7722@naver.com>
2025-03-12 10:11:30 +0900

    shop 프로그램 완성

--- shop/src/shop/MyShop.java
+++ shop/src/shop/MyShop.java
@@ -7,6 +7,8 @@ public class MyShop implements IShop {
      private String title;
      private User[] users = new User[5];
      private Product[] products = new Product[10];
+     private String seluser;
+     private Product[] carts = new Product[10];
      Scanner sc = new Scanner(System.in);
 
      @Override
@@ -49,40 +51,76 @@ public class MyShop implements IShop {
           System.out.println("[x] 종료");
           System.out.println("===============================");
           System.out.print("선택 : ");
-          String select = sc.nextLine();
-          char seluser = select.charAt(0);
-          if (seluser == 'x' || seluser == 'X') {
+          seluser = sc.nextLine();
+          if (seluser.equalsIgnoreCase("x")) {
                System.out.println("가게를 닫습니다.");
-          } else if ('0' <= seluser && seluser <= '9') {
-               System.out.println(users[(int) seluser - 48].getName() + "님 안녕하세요.");
+          } else if (users[Integer.parseInt(seluser)] != null) {
+               System.out.println(users[Integer.parseInt(seluser)].getName() + "님 안녕하세요.");
                productList();
           }
      }
 
      public void productList() {
-          System.out.println(this.title + " : 상품목록 - 상품선택");
-          System.out.println("===============================");
-          for (int i = 0; i < products.length; i++) {
-               if (products[i] != null) {
-                    System.out.println("[" + i + "] " + products[i].getPname());
-                    System.out.println("    가격 : " + products[i].getPrice());
-                    System.out.print("    ");
-                    products[i].printExtra();
+          int empty = 0;
+          boolean run = true;
+          while (run) {
+               System.out.println(this.title + " : 상품목록 - 상품선택");
+               System.out.println("===============================");
+               for (int i = 0; i < products.length; i++) {
+                    if (products[i] != null) {
+                         System.out.println("[" + i + "] " + products[i].getPname());
+                         System.out.println("    가격 : " + products[i].getPrice());
+                         System.out.print("    ");
+                         products[i].printExtra();
+                    }
+               }
+               System.out.println("[H] 메인화면");
+               System.out.println("[C] 체크아웃");
+               System.out.println("===============================");
+               System.out.print("선택 : ");
+               String selprod = sc.nextLine();
+               if (selprod.equalsIgnoreCase("h")) {
+                    System.out.println("메인화면으로 돌아갑니다.");
+                    run = false;
+                    start();
+               } else if (selprod.equalsIgnoreCase("c")) {
+                    System.out.println("체크아웃하셨습니다.");
+                    run = false;
+                    checkout();
+               } else if (products[Integer.parseInt(selprod)] != null) {
+                    System.out.println(products[Integer.parseInt(selprod)].getPname() + "을 선택하셨습니다.");
+                    for (int i = 0; i < carts.length; i++) {
+                         if (carts[i] == null) {
+                              empty = i;
+                              break;
+                         }
+                    }
+                    carts[empty] = products[Integer.parseInt(selprod)];
                }
           }
-          System.out.println("[h] 메인화면");
-          System.out.println("[c] 체크아웃");
+
+     }
+
+     public void checkout() {
+          int sum = 0;
+          System.out.println(this.title + " : " + users[Integer.parseInt(seluser)].getName() + " - 체크아웃");
+          System.out.println("===============================");
+          for (int i = 0; i < carts.length; i++) {
+               if (carts[i] != null) {
+                    System.out.println("[" + i + "] " + carts[i].getPname() + "     " + carts[i].getPrice());
+               }
+          }
+          System.out.println("[B] 이전화면");
+          System.out.println("[Q] 종료");
           System.out.println("===============================");
           System.out.print("선택 : ");
           String select = sc.nextLine();
-          char selprod = select.charAt(0);
-          if (selprod == 'h' || selprod == 'H') {
-               System.out.println("메인화면으로 돌아갑니다.");
-          } else if (selprod == 'c' || selprod == 'C') {
-               System.out.println("체크아웃하셨습니다.");
-          } else if ('0' <= selprod && selprod <= '9') {
-               System.out.println(products[0].getPname() + "을 선택하셨습니다.");
+          if (select.equalsIgnoreCase("b")) {
+               System.out.println("이전화면으로 돌아갑니다.");
+               productList();
+          } else if (select.equalsIgnoreCase("q")) {
+               System.out.println("종료합니다.");
+               System.exit(sum);
           }
      }
-
 }
